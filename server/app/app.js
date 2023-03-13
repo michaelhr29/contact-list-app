0529e747467c9693c2e63c9dd9991f14d09822a6
Michael Hernandez <michaeladrian.hernandez.rala@gmail.com>
2023-03-13 19:18:41 +0100

    [Server} - Some fixes

--- server/app/app.js
+++ server/app/app.js
@@ -1,16 +1,16 @@
 const app = require('express')();
 require('dotenv').config();
 
-const Logger = require('./utils/logger');
-const authHandler = require('./middlewares/authHandler');
-const errorHandler = require('./middlewares/errorHandler');
-
 const fs = require('fs');
 const path = require('path');
 const YAML = require('js-yaml');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const OpenApiValidator = require('express-openapi-validator');
+
+const Logger = require('./utils/logger');
+const authHandler = require('./middlewares/authHandler');
+const errorHandler = require('./middlewares/errorHandler');
 
 const apiSpec = path.join(__dirname, 'openapi', 'api.yml');
 const swaggerDocument = YAML.load(fs.readFileSync(apiSpec));
@@ -34,5 +34,5 @@ app.use(
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const { PORT } = process.env;
 app.listen(PORT, () => Logger.info({ message: `Server listening on PORT ${PORT}` }));
--- server/app/middlewares/errorHandler.js
+++ server/app/middlewares/errorHandler.js
@@ -3,6 +3,7 @@ const ResponseHelper = require('../utils/responseHelper');
 module.exports = (err, req, res, next) => {
   const statusCode = err.status;
   console.log(err);
+  // eslint-disable-next-line prefer-destructuring
   const { message } = err.errors[0];
 
   ResponseHelper.error(req, res, statusCode, message);
